2324da8c5f66191b45c1034528297e4c86f87283
Even Grønbech-Hope <evengrhope@outlook.com>
2023-04-04 14:30:13 +0200

    update

--- Dark_Theme.R
+++ Dark_Theme.R
@@ -8,9 +8,13 @@ dark_theme <- function() {
       plot.background = element_rect(fill = "gray10"),
       plot.margin = unit(c(5, 10, 5, 10), units = "mm"),
       plot.title = element_text(face = "bold.italic", color = "white"),
+      plot.subtitle = element_text(face = "bold.italic", color = "gray80"),
       axis.title = element_text(face = "bold", color = "white"),
       axis.text = element_text(color = "white"),
       legend.text = element_text(color = "white"),
       legend.title = element_text(face = "bold", color = "white"),
       panel.grid = element_line(color = "gray40")
     ) }
+
+# Colors that goes with the theme.
+colors = c("#ff2800", "#00A19B", "#C0BFBF", "#FDD900", "#294643", "#FF8000", "#2173B8")
